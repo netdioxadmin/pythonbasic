1de18c19f41cc02548c5d15c89e63390f7519c9d
netdioxadmini <netdioxadmini@gmail.com>
2022-07-12 04:03:52 +0000

    print test

---
+++ print.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+print ("Hello Welcome to TUTORIAL")
+
+num1 = input("Enter your first number:   ")
+num2 = input ("Enter your second number:  ")
+
+sum = int(num1) + int(num2)
+
+print ( " The sum of the entered nos are " + str(sum))
+
+#printing sums and from variable
